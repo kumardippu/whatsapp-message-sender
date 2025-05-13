77b5fdc133a7651f58cb57928c41fc5a89b2ca49
Dippu Kumar <kumardippu@gmail.com>
2025-05-13 21:31:45 +0530

    Update wh.py

--- wh.py
+++ wh.py
@@ -2,7 +2,7 @@
 import pywhatkit
 # syntax: phone number with country code, message, hour and minutes
 pywhatkit.sendwhatmsg('+91******850', 'Dippu is here.........',10,36)
-#pywhatkit.sendwhatmsg('+917503834850', 'Dippu is here.........', 22, 14)
+#pywhatkit.sendwhatmsg('+91*****850', 'Dippu is here.........', 22, 14)
 
 '''import pywhatkit
 #syntax: group link, message, hour and minutes
